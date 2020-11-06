a027880a8820ba3d61e79c41996dc7b8940e28cd
LENAERTS Tom <t.lenaerts@laposte.net>
2020-11-06 12:02:58 +0100

    General Audio

---
+++ Assets/T4_SCRIPTS/T4_AudioMananger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T4_AudioMananger : MonoBehaviour
+{
+    public T4_GameManager gameManager;
+    AudioSource audioSource;
+    public AudioClip levelAudio;
+    public AudioClip winAudio;
+    public AudioClip loseAudio;
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (gameManager.hasFinished == true)
+        {
+            if (gameManager.hasWin == true && audioSource.clip != winAudio)
+            {
+                audioSource.loop = false;
+                audioSource.clip = winAudio;
+                audioSource.Play(0);
+            }
+            else if (gameManager.hasWin == false && audioSource.clip != loseAudio)
+            {
+                audioSource.loop = false;
+                audioSource.clip = loseAudio;
+                audioSource.Play(0);
+            }
+        }
+
+        else if (audioSource.clip != levelAudio) 
+        {
+            audioSource.loop = true;
+            audioSource.clip = levelAudio;
+            audioSource.Play(0);
+        }
+    }
+}
---
+++ Assets/T4_SCRIPTS/T4_FlamethrowerAudio.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class T4_FlamethrowerAudio : MonoBehaviour
+{
+    #region Variables
+    public AudioClip startAudio;
+    public AudioClip loop;
+    AudioSource audioS;
+
+    #endregion
+    void Awake()
+    {
+        audioS = GetComponent<AudioSource>();
+    }
+
+    private void OnEnable()
+    {
+        audioS.clip = startAudio;
+        audioS.Play();
+    }
+
+    void Update()
+    {
+        if (!audioS.isPlaying)
+        {
+            audioS.clip = loop;
+            audioS.Play();
+        }
+    }
+}
